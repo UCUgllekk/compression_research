5df60ee9f7f988a1357c42fc88b4bc5f161f60f4
Roman Pavlosiuk <romanpavlosiuk10@gmail.com>
2024-05-15 05:24:46 +0300

    final pip

--- build/lib/algorithms/__init__.py
+++
@@ -1,5 +0,0 @@
-from algorithms.lzw import LZW
-from algorithms.huffman import HuffmanCompression
-from algorithms.lz77 import LZ77
-from algorithms.deflate import Deflate
-from algorithms.lz78 import LZ78
